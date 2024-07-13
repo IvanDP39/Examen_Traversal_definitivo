828f6a403fb28aa216010a1552923b640a5312d3
IvanDP39 <i.delpino@duocuc.cl>
2024-07-12 21:25:41 -0400

    Update Examen_Tranversal.py

---
+++ Examen_Tranversal.py
@@ -0,0 +1,28 @@
+
+from Funciones_Examen_Tranversal import *
+while True:
+ 
+ menu()
+ opcion= int(input("Porfavor seleccione una opcion: "))
+
+ if opcion == 1:
+  Asignacion_de_Sueldos()
+  print("La asignacion de sueldo fue realizada con exito!")
+
+ elif opcion == 2:
+  print("")
+
+ elif opcion == 3:
+  print("")
+
+ elif opcion == 4:
+  print("")
+
+ elif opcion == 5:
+  print("Saliendo del Programa....")
+  print("Desarrollado por Ivan Del Pino")
+  print("Rut: 20.637.397-0")
+  break
+ 
+ else:
+  print("Se espera un numero intente nuevamente")
